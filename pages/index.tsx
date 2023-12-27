02e26cf53956ab528f7247fd1d00ab7690154b23
fayzezy <154333642+fayzezy@users.noreply.github.com>
2023-12-27 06:11:00 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,102 @@ import { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <main className={styles.main}>
-       <h1>oxstaking</h1>
-       <web3Button>claim H</web3Button>
+       <div className={styles.container}>
+        <div className={styles.header}>
+        <h1 className={styles.title}>
+            Welcome to{""}
+            <span className={styles.gradientText0}>
+              <a
+                href="https://thirdweb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                thirdweb.
+              </a>
+            </span>
+          </h1>
+
+          <p className={styles.description}>
+            Stake Your 0x Domain to get H Token{" "}
+            <code className={styles.code}>src/index.js</code>, {" "}
+            <code className={styles.code}>src/App.js</code> 
+          </p>
+
+          <div className={styles.connect}>
+            <ConnectWallet
+              dropdownPosition={{
+                side: "bottom",
+                align: "center",
+              }}
+            />
+          </div>
+        </div>
+
+        <div className={styles.grid}>
+          <a
+            href="https://portal.thirdweb.com/"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/images/portal-preview.png"
+              alt="Placeholder preview of starter"
+              width={300}
+              height={200}
+            />
+            <div className={styles.cardText}>
+              <h2 className={styles.gradientText1}>Portal ➜</h2>
+              <p>
+                Guides, references, and resources that will help you build with
+                thirdweb.
+              </p>
+            </div>
+          </a>
+
+          <a
+            href="https://thirdweb.com/dashboard"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/images/dashboard-preview.png"
+              alt="Placeholder preview of starter"
+              width={300}
+              height={200}
+            />
+            <div className={styles.cardText}>
+              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
+              <p>
+                Deploy, configure, and manage your smart contracts from the
+                dashboard.
+              </p>
+            </div>
+          </a>
+
+          <a
+            href="https://thirdweb.com/templates"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/images/templates-preview.png"
+              alt="Placeholder preview of templates"
+              width={300}
+              height={200}
+            />
+            <div className={styles.cardText}>
+              <h2 className={styles.gradientText3}>Templates ➜</h2>
+              <p>
+                Discover and clone template projects showcasing thirdweb
+                features.
+              </p>
+            </div>
+          </a>
+        </div>
+      </div>
     </main>
   );
 };
